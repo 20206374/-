d2fc0cdb6c38bec64925dd74c7118baf6e0561a3
M萌superman <104714310+20206374@users.noreply.github.com>
2022-12-27 22:50:11 +0800

    Update mycpu_core.v

--- SampleCPU/mycpu_core.v
+++ SampleCPU/mycpu_core.v
@@ -28,7 +28,12 @@ module mycpu_core(
     wire [`BR_WD-1:0] br_bus; 
     wire [`DATA_SRAM_WD-1:0] ex_dt_sram_bus;
     wire [`WB_TO_RF_WD-1:0] wb_to_rf_bus;
+    wire [`EX_TO_RF_WD-1:0] ex_to_rf_bus;
+    wire [`MEM_TO_RF_WD-1:0] mem_to_rf_bus;
     wire [`StallBus-1:0] stall;
+    wire stallreq_for_load;
+    wire stallreq_for_bru;
+    wire stallreq_for_ex;
 
     IF u_IF(
     	.clk             (clk             ),
@@ -47,24 +52,29 @@ module mycpu_core(
     	.clk             (clk             ),
         .rst             (rst             ),
         .stall           (stall           ),
-        .stallreq        (stallreq        ),
         .if_to_id_bus    (if_to_id_bus    ),
         .inst_sram_rdata (inst_sram_rdata ),
         .wb_to_rf_bus    (wb_to_rf_bus    ),
+        .ex_to_rf_bus    (ex_to_rf_bus    ),
+        .mem_to_rf_bus   (mem_to_rf_bus   ),
         .id_to_ex_bus    (id_to_ex_bus    ),
-        .br_bus          (br_bus          )
+        .br_bus          (br_bus          ),
+        .stallreq_for_load (stallreq_for_load),
+        .stallreq_for_bru (stallreq_for_bru)
     );
 
     EX u_EX(
-    	.clk             (clk             ),
-        .rst             (rst             ),
-        .stall           (stall           ),
-        .id_to_ex_bus    (id_to_ex_bus    ),
-        .ex_to_mem_bus   (ex_to_mem_bus   ),
-        .data_sram_en    (data_sram_en    ),
-        .data_sram_wen   (data_sram_wen   ),
-        .data_sram_addr  (data_sram_addr  ),
-        .data_sram_wdata (data_sram_wdata )
+    	.clk               (clk             ),
+        .rst               (rst             ),
+        .stall             (stall           ),
+        .id_to_ex_bus      (id_to_ex_bus    ),
+        .ex_to_mem_bus     (ex_to_mem_bus   ),
+        .data_sram_en      (data_sram_en    ),
+        .data_sram_wen     (data_sram_wen   ),
+        .data_sram_addr    (data_sram_addr  ),
+        .ex_to_rf_bus      (ex_to_rf_bus    ),
+        .stallreq_for_ex   (stallreq_for_ex ),
+        .data_sram_wdata   (data_sram_wdata )
     );
 
     MEM u_MEM(
@@ -73,6 +83,7 @@ module mycpu_core(
         .stall           (stall           ),
         .ex_to_mem_bus   (ex_to_mem_bus   ),
         .data_sram_rdata (data_sram_rdata ),
+        .mem_to_rf_bus   (mem_to_rf_bus   ),
         .mem_to_wb_bus   (mem_to_wb_bus   )
     );
     
@@ -89,8 +100,11 @@ module mycpu_core(
     );
 
     CTRL u_CTRL(
-    	.rst   (rst   ),
-        .stall (stall )
+    	.rst               (rst              ),
+        .stall             (stall            ),
+        .stallreq_for_bru  (stallreq_for_bru ),
+        .stallreq_for_ex   (stallreq_for_ex  ),
+        .stallreq_for_load (stallreq_for_load)
     );
     
 endmodule
